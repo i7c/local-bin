2a36c90914ea9b6d28b1837308048761835488d6
Constantin Weißer <constantin.weisser@novatec-gmbh.de>
2017-09-21 12:16:43 +0200

    New audio device

--- jabra.sh
+++ jabra.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Sets my jabra device as default audio device in pulse.
-pactl set-default-source alsa_input.usb-GN_Netcom_A_S_Jabra_EVOLVE_LINK_MS_0000288E957E07-00.analog-mono
-pactl set-default-sink alsa_output.usb-GN_Netcom_A_S_Jabra_EVOLVE_LINK_MS_0000288E957E07-00.analog-stereo
+pactl set-default-source alsa_input.usb-GN_Netcom_A_S_Jabra_EVOLVE_LINK_MS_00023B7ED69E07-00.analog-mono
+pactl set-default-sink alsa_output.usb-GN_Netcom_A_S_Jabra_EVOLVE_LINK_MS_00023B7ED69E07-00.analog-stereo
