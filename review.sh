2be006f18de30a139563afb38527b87690db5351
Constantin Weißer <i7c@posteo.de>
2017-09-25 00:38:16 +0200

    Add "seen indicator" to review.sh
    review.sh now shows an indicator if you already inspected a commit or
    not.

--- review.sh
+++ review.sh
@@ -1,27 +1,52 @@
 #!/bin/bash
 
+seen_file=$(mktemp)
+
+seen() {
+    grep -q "$@" "$seen_file";
+}
+
+saw() {
+    if ! seen "$@"; then
+        echo "$@" >> $seen_file;
+    fi
+}
+
+list_revs() {
+    local prefix;
+    while read -r r; do
+        if seen "$r"; then
+            git log --pretty='%H [X] - %s (%D)' "$r" -1;
+        else
+            git log --pretty='%H [ ] - %s (%D)' "$r" -1;
+        fi
+    done <<< $revlist;
+}
+
 if [[ "$1" == "_" ]]; then
     shift;
     revlist="$(git log --color --graph --pretty="%H %s (%D)" "$@" -1024 | \
         fzf --reverse -m --ansi | \
-        sed -r 's,^[| *]*([a-z0-9]* .*)$,\1,' | grep -P '\S')"
+        sed -r 's,^[| *]*([a-z0-9]*).*$,\1,' | grep -P '\S')"
 elif [[ "$1" ]]; then
-    revlist="$(git log --reverse --format='%H - %s' "$@")";
+    revlist="$(git log --reverse --format='%H' "$@")";
 else
-    revlist="$(git log --reverse --format='%H - %s' "origin/master..HEAD")";
+    revlist="$(git log --reverse --format='%H' "origin/master..HEAD")";
 fi
-# Obtain revlist to consider at all
 
 # Prompt for commit and inspect it in a loop until the user wants to exit
 commit="-";
 while true; do
-    commit=$(echo "$revlist" | \
+    commit=$(list_revs | \
         fzf --reverse --header "Select commit to inspect. Last was $commit" | \
         awk '{print $1}');
     if [[ ! "$commit" ]]; then
         break;
     fi
+    saw "$commit";
     clear;
     inspect_commit.sh "$commit";
 done;
+
+rm "$seen_file";
 exit 0;
